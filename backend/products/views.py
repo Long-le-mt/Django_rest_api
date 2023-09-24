a07ddf711d4f97793d1426b05cbe79657b188ba7
Long Nhật <longle.24hdev@gmail.com>
2023-09-25 01:41:36 +0700

    ListCreateAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -6,6 +6,19 @@ from .models import Product
 from .serializers import ProductSerializer
 
 
+class ProductListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+        # send a Django signal
+        
 class ProductCreateAPIView(generics.CreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
