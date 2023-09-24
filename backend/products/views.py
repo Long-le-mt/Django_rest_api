5276327e19b5adc99194a2e8f5b35a99d5c61944
Long Nhật <longle.24hdev@gmail.com>
2023-09-25 01:43:21 +0700

    Using function based view for create retrieve or list

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
-from rest_framework import mixins
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+# from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -18,7 +22,7 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
             content = title
         serializer.save(content=content)
         # send a Django signal
-        
+
 class ProductCreateAPIView(generics.CreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
@@ -42,3 +46,30 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     #     serializer = self.get_serializer(instance)
     #     print("Hello", serializer.data)
     #     return self.retrieve(request, *args, **kwargs)
+
+@api_view(['GET', 'POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method 
+
+    if method == "GET":
+        if pk is not None:
+            # detail view
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
+        # list view
+        queryset = Product.objects.all() 
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+
+    if method == "POST":
+        # create an item
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response({"invalid": "not good data"}, status=400)
