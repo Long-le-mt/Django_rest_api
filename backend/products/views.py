40e7109736d2dec8cc9713c6ac760807f6e488f1
Long Nhật <longle.24hdev@gmail.com>
2023-09-25 01:32:27 +0700

    CreateAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -6,6 +6,19 @@ from .models import Product
 from .serializers import ProductSerializer
 
 
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    # nạp chồng phương thức perform_create
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
