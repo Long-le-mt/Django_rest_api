752bf9f25daac9f14b727b2481c2ece999844d5f
Long Nhật <longle.24hdev@gmail.com>
2023-10-01 15:57:55 +0700

    Mixin class base view

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     # path('', views.ProductCreateAPIView.as_view()),
     path('', views.ProductListCreateAPIView.as_view()),
-    path('<int:pk>/', views.ProductDetailAPIView.as_view())
+    path('<int:pk>/update/', views.ProductUpdateAPIView.as_view()),
+    path('<int:pk>/', views.ProductDetailAPIView.as_view()),
+    path('mixin', views.ProductMixinView.as_view()),
+    path('mixin/<int:pk>', views.ProductMixinView.as_view())
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,8 +1,6 @@
-from rest_framework import generics
-from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from rest_framework import generics, mixins
 # from django.http import Http404
 from django.shortcuts import get_object_or_404
 
@@ -47,6 +45,16 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     #     print("Hello", serializer.data)
     #     return self.retrieve(request, *args, **kwargs)
 
+class ProductUpdateAPIView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+            
 @api_view(['GET', 'POST'])
 def product_alt_view(request, pk=None, *args, **kwargs):
     method = request.method 
@@ -73,3 +81,30 @@ def product_alt_view(request, pk=None, *args, **kwargs):
             serializer.save(content=content)
             return Response(serializer.data)
         return Response({"invalid": "not good data"}, status=400)
+    
+class ProductMixinView(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView
+    ):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def get(self, request, *args, **kwargs): #HTTP -> get
+        pk = kwargs.get("pk")
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = "this is a single view doing cool stuff"
+        serializer.save(content=content)
